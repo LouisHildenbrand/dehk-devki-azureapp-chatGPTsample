1e6b15faa68f2b7d49defaa7fdeac982cbe0e38b
lohil <lohil@outlook.de>
2024-02-19 13:35:02 +0100

    Button tests

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -48,19 +48,31 @@ interface HistoryButtonProps extends IButtonProps {
 export const HistoryButton: React.FC<HistoryButtonProps> = ({onClick, text}) => {
     const historyButtonStyles: ICommandBarStyles & IButtonStyles = {
         root: {
-            width: '180px',
-            border: `1px solid #D1D1D1`,
-          },
-          rootHovered: {
-            border: `1px solid #D1D1D1`,
-          },
-          rootPressed: {
-            border: `1px solid #D1D1D1`,
-          },
+          width: '180px',
+          height: 32,
+          borderRadius: 4,
+          background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)',
+        //   position: 'absolute',
+        //   right: 20,
+          padding: '5px 12px',
+          marginRight: '20px'
+        },
+        icon: {
+          color: '#FFFFFF',
+        },
+        rootHovered: {
+          background: 'linear-gradient(135deg, #0F6CBD 0%, #2D87C3 51.04%, #8DDDD8 100%)',
+        },
+        label: {
+          fontWeight: 600,
+          fontSize: 14,
+          lineHeight: '20px',
+          color: '#FFFFFF',
+        }
       };
 
       return (
-        <DefaultButton
+        <CommandBarButton
             text={text}
             iconProps={{ iconName: 'History' }}
             onClick={onClick}
